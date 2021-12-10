7570c1ea7797a9692b0032e3bb2c7247ed33e6bb
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-12-11 00:19:03 +0100

    Day 7 part 2.

--- src/day07.zig
+++ src/day07.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const testing = std.testing;
-const assert = std.debug.assert;
 
 const log = std.log.scoped(.day07);
 
@@ -12,12 +11,16 @@ pub fn run(allocator: *std.mem.Allocator) anyerror!void {
     while (it.next()) |v| try initial_positions.append(try std.fmt.parseInt(i32, v, 10));
 
     var fuel: i32 = 0;
-    var part1_pos = findOptimalAlignmentPosition(initial_positions.items, &fuel);
-
+    var part1_pos = findOptimalAlignmentPosition(initial_positions.items, &fuel, .part1);
     log.info("Part 1: pos {d}, fuel {d}", .{ part1_pos, fuel });
+
+    var part2_pos = findOptimalAlignmentPosition(initial_positions.items, &fuel, .part2);
+    log.info("Part 2: pos {d}, fuel {d}", .{ part2_pos, fuel });
 }
 
-pub fn findOptimalAlignmentPosition(initial_positions: []const i32, fuel: *i32) i32 {
+const Method = enum { part1, part2 };
+
+pub fn findOptimalAlignmentPosition(initial_positions: []const i32, fuel: *i32, comptime method: Method) i32 {
     var max_value: i32 = 0;
     for (initial_positions) |v| max_value = std.math.max(v, max_value);
 
@@ -26,7 +29,20 @@ pub fn findOptimalAlignmentPosition(initial_positions: []const i32, fuel: *i32) 
     var target: i32 = 0;
     while (target <= max_value) : (target += 1) {
         var cost: i32 = 0;
-        for (initial_positions) |pos| cost += std.math.absInt(target - pos) catch unreachable;
+
+        switch (method) {
+            .part1 => {
+                for (initial_positions) |pos|
+                    cost += std.math.absInt(target - pos) catch unreachable;
+            },
+            .part2 => {
+                // Use binomial coefficient n*(n+1)/2
+                for (initial_positions) |pos| {
+                    var n = std.math.absInt(target - pos) catch unreachable;
+                    cost += @divTrunc(n * (n + 1), 2);
+                }
+            },
+        }
         if (cost < best_cost) {
             best_pos = target;
             best_cost = cost;
@@ -41,8 +57,18 @@ test "part 1" {
     const example = [_]i32{ 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
 
     var fuel: i32 = 0;
-    var optimal_position = findOptimalAlignmentPosition(&example, &fuel);
+    var optimal_position = findOptimalAlignmentPosition(&example, &fuel, .part1);
 
     try testing.expectEqual(optimal_position, 2);
     try testing.expectEqual(fuel, 37);
+}
+
+test "part 2" {
+    const example = [_]i32{ 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 };
+
+    var fuel: i32 = 0;
+    var optimal_position = findOptimalAlignmentPosition(&example, &fuel, .part2);
+
+    try testing.expectEqual(optimal_position, 5);
+    try testing.expectEqual(fuel, 168);
 }
