f17977b4d3696b8d3a6ce1e46452f0bc62b5f352
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-12-10 01:30:17 +0100

    Day 06 part 2.

--- src/day06.zig
+++ src/day06.zig
@@ -9,34 +9,47 @@ pub fn run(allocator: *std.mem.Allocator) anyerror!void {
     var initialState = try parseInputStrAlloc(allocator, inputStr);
     defer allocator.free(initialState);
 
-    var result = try simulateNumFish(allocator, initialState, 80);
-
-    log.info("Part 1: Number of lanternfish after 80 days: {d}", .{result});
+    log.info("Part 1: Number of lanternfish after 80 days: {d}", .{try simulateNumFish(initialState, 80)});
+    log.info("Part 2: Number of lanternfish after 256 days: {d}", .{try simulateNumFish(initialState, 256)});
 }
 
-pub fn simulateNumFish(allocator: *std.mem.Allocator, initialState: []u8, numDays: usize) !usize {
-    var stateList = std.ArrayList(u8).init(allocator);
-    defer stateList.deinit();
-    try stateList.appendSlice(initialState);
+pub fn simulateNumFish(initialState: []u8, numDays: usize) !usize {
+
+    // There are 9 states 0,1,2,...,8
+    var states = [_]usize{0} ** 9;
+    for (states) |state, i| {
+        for (initialState) |fishState| {
+            states[i] += @boolToInt(fishState == i);
+        }
+    }
 
     var iDay: usize = 0;
     while (iDay < numDays) : (iDay += 1) {
-        var numNewFishToCreate: usize = 0;
-        for (stateList.items) |counter, i| {
-            if (counter == 0) {
-                stateList.items[i] = 6;
-                numNewFishToCreate += 1;
+        // Simulate the days for each group
+        var numResetFish: usize = 0;
+
+        // Age all fish
+        var iState: usize = 0;
+        while (iState <= 8) : (iState += 1) {
+            if (iState == 0) {
+                // These fish are moved to 6
+                numResetFish = states[0];
+                states[0] = 0;
             } else {
-                stateList.items[i] -= 1;
+                states[iState - 1] = states[iState];
+                states[iState] = 0;
             }
         }
 
-        // Extend our states vector and create new fish.
-        // New fish have counter set to 8.
-        try stateList.appendNTimes(8, numNewFishToCreate);
+        // Add the reset fish to group 6 and create new fish in group 8
+        states[6] += numResetFish;
+        states[8] += numResetFish;
     }
 
-    return stateList.items.len;
+    var numFish: usize = 0;
+    for (states) |state| numFish += state;
+
+    return numFish;
 }
 
 pub fn parseInputStrAlloc(allocator: *std.mem.Allocator, str: []const u8) ![]u8 {
@@ -60,6 +73,14 @@ test "part 1" {
     defer testing.allocator.free(initialState);
 
     // In this example, after 18 days, there are a total of 26 fish. After 80 days, there would be a total of 5934.
-    try testing.expectEqual(try simulateNumFish(testing.allocator, initialState, 18), 26);
-    try testing.expectEqual(try simulateNumFish(testing.allocator, initialState, 80), 5934);
+    try testing.expectEqual(try simulateNumFish(initialState, 18), 26);
+    try testing.expectEqual(try simulateNumFish(initialState, 80), 5934);
+}
+
+test "part 2" {
+    const example = "3,4,3,1,2";
+    var initialState = try parseInputStrAlloc(testing.allocator, example);
+    defer testing.allocator.free(initialState);
+
+    try testing.expectEqual(try simulateNumFish(initialState, 256), 26984457539);
 }
