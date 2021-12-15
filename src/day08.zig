55107bd020871d1dcefca1b08d5d86fd5702c37d
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-12-15 22:14:13 +0100

    Day 8 part 2.

--- src/day08.zig
+++ src/day08.zig
@@ -9,14 +9,12 @@ pub fn run() anyerror!void {
     var num_easy_digits: u32 = countNumberOfEasyDigits(input_str);
     log.info("Part 1: {d}", .{num_easy_digits});
 
-    _ = crackSegmentMapping(short_example);
-    //var a: [7]u8 = .{ 0, 1, 2, 3, 4, 5, 6 };
-    //var i: usize = 0;
-    //while (i < 12) : (i += 1) {
-    //    generatePermutation(7, &a);
-    //    for (a) |v| std.debug.print(" {d}", .{v});
-    //    std.debug.print("\n", .{});
-    //}
+    var sum: u32 = 0;
+    var line_it = std.mem.tokenize(input_str, "\n");
+    while (line_it.next()) |line| {
+        sum += deduceOutput(line);
+    }
+    log.info("Part 2: sum {d}", .{sum});
 }
 
 pub fn countNumberOfEasyDigits(input: []const u8) u32 {
@@ -47,44 +45,11 @@ pub fn countNumberOfEasyDigits(input: []const u8) u32 {
 // e    f
 //  gggg
 // We store these as bitset
-// a b c d e f g -> 0 1 2 3 4 5 6
+// a b c d e f g -> (index) 0 1 2 3 4 5 6
 
-const Signal = enum {
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
+const Signal = enum(u7) { a = 0b1000000, b = 0b0100000, c = 0b0010000, d = 0b0001000, e = 0b0000100, f = 0b0000010, g = 0b0000001 };
 
-    pub fn fromChar(char: u8) Signal {
-        return switch (char) {
-            'a' => .a,
-            'b' => .b,
-            'c' => .c,
-            'd' => .d,
-            'e' => .e,
-            'f' => .f,
-            'g' => .g,
-            else => unreachable,
-        };
-    }
-
-    pub fn asBitCode(self: Signal) u7 {
-        return switch (self) {
-            .a => 0b1000000,
-            .b => 0b0100000,
-            .c => 0b0010000,
-            .d => 0b0001000,
-            .e => 0b0000100,
-            .f => 0b0000010,
-            .g => 0b0000001,
-        };
-    }
-};
-
-const segments_used_in_digit: [10]u7 = .{
+const original_digit_signals: [10]u7 = .{
     0b1110111, // 0: a,b,c,e,f,g
     0b0010010, // 1: c,f
     0b1011101, // 2: a,c,d,e,g
@@ -99,48 +64,14 @@ const segments_used_in_digit: [10]u7 = .{
 
 fn signalStrToBitMap(signal: []const u8) u7 {
     var bitmap: u7 = 0b0000000;
-
-    for (signal) |char, i| {
-        bitmap |= @enumToInt(std.meta.stringToEnum(Signal, signal[i .. i + 1]).?);
-    }
+    for (signal) |_, i| bitmap |= @enumToInt(std.meta.stringToEnum(Signal, signal[i .. i + 1]).?);
     return bitmap;
 }
 
-// Heap's algorithm
-// https://en.wikipedia.org/wiki/Heap's_algorithm
-pub fn generatePermutation(k: usize, a: []u8) void {
-    if (k == 1) return;
+fn deduceOutput(signal_output_str: []const u8) u32 {
+    var scrambled_digit_signals = [_]?u7{null} ** 10;
 
-    generatePermutation(k - 1, a);
-
-    var i: usize = 0;
-    while (i < k - 1) : (i += 1) {
-        if (k % 2 == 0) {
-            std.mem.swap(u8, &a[i], &a[k - 1]);
-        } else {
-            std.mem.swap(u8, &a[0], &a[k - 1]);
-        }
-        generatePermutation(k - 1, a);
-    }
-}
-
-fn mapSignal(signal: u7, mapping: [7]u8) u7 {
-    var mapped_signal: u7 = 0;
-
-    mapped_signal |= (signal & Signal.a.asBitCode()) >> @intCast(u3, mapping[0]);
-    mapped_signal |= (signal & Signal.b.asBitCode()) >> @intCast(u3, mapping[1]);
-    mapped_signal |= (signal & Signal.c.asBitCode()) >> @intCast(u3, mapping[2]);
-    mapped_signal |= (signal & Signal.d.asBitCode()) >> @intCast(u3, mapping[3]);
-    mapped_signal |= (signal & Signal.e.asBitCode()) >> @intCast(u3, mapping[4]);
-    mapped_signal |= (signal & Signal.f.asBitCode()) >> @intCast(u3, mapping[5]);
-    mapped_signal |= (signal & Signal.g.asBitCode()) >> @intCast(u3, mapping[6]);
-
-    return mapped_signal;
-}
-
-fn crackSegmentMapping(signal_output_str: []const u8) void {
     var it = std.mem.tokenize(signal_output_str, " |");
-
     var signals: [10]u7 = .{0} ** 10;
     {
         var i: usize = 0;
@@ -153,62 +84,78 @@ fn crackSegmentMapping(signal_output_str: []const u8) void {
         while (i < 4) : (i += 1) output[i] = signalStrToBitMap(it.next().?);
     }
 
-    // Iterate over possible mappings.
-    var mapping: [7]u8 = .{ 0, 1, 2, 3, 4, 5, 6 };
+    var remaining_digits: usize = 10;
 
-    var i_mapping: usize = 0;
-    while (i_mapping < 7 * 6 * 5 * 4 * 3 * 2 * 1) : (i_mapping += 1) {
-        for (mapping) |m| std.debug.print("{d} ", .{m});
-        std.debug.print("\n", .{});
+    while (remaining_digits > 0) {
         for (signals) |signal| {
-            var easy_digit: ?u8 = switch (@popCount(u7, signal)) {
-                2 => 1,
-                3 => 7,
-                4 => 4,
-                7 => 8,
-                else => null,
-            };
-
-            if (easy_digit != null) {
-                // map signal and compare with segment
-                var mapped_signal = mapSignal(signal, mapping);
-                for (segments_used_in_digit) |expected_signal| {
-                    log.info("0b{b:0>7} -> 0b{b:0>7}", .{ signal, mapped_signal });
-                    if (mapped_signal == expected_signal) {
-                        log.info("match! 0b{b:0>7} -> 0b{b:0>7}", .{ signal, mapped_signal });
+            // Use length of signal to determine possible ...
+            var decoded_digit: ?usize = null;
+            switch (@popCount(u7, signal)) {
+                2 => decoded_digit = 1,
+                3 => decoded_digit = 7,
+                4 => decoded_digit = 4,
+                5 => {
+                    // It could be either 2, 3 or 5.
+                    // Intersect with signals we've already found, and see if we can deduce from that.
+                    // 3 x 1 => 2 shared segments
+                    // 2 x 4 => 2 shared segment
+                    // 5 x 4 => 3 shared segments
+                    if (scrambled_digit_signals[1]) |one| {
+                        if (@popCount(u7, one & signal) == 2) {
+                            decoded_digit = 3;
+                        } else if (scrambled_digit_signals[4]) |four| {
+                            if (@popCount(u7, four & signal) == 2) {
+                                decoded_digit = 2;
+                            } else if (@popCount(u7, four & signal) == 3) {
+                                decoded_digit = 5;
+                            }
+                        }
                     }
-                }
+                },
+                6 => {
+                    // It could be either 0, 6 or 9.
+                    // Intersect with signals we've already found, and see if we can deduce from that.
+                    // 6 x 1 => 1 shared segments
+                    // 0 x 4 => 3 shared segments
+                    // 9 x 4 => 5 shared segments
+                    if (scrambled_digit_signals[1]) |one| {
+                        if (@popCount(u7, one & signal) == 1) {
+                            decoded_digit = 6;
+                        } else if (scrambled_digit_signals[4]) |four| {
+                            if (@popCount(u7, four & signal) == 3) {
+                                decoded_digit = 0;
+                            } else if (@popCount(u7, four & signal) == 4) {
+                                decoded_digit = 9;
+                            }
+                        }
+                    }
+                },
+                7 => decoded_digit = 8,
+                else => unreachable,
+            }
+
+            if (decoded_digit != null and scrambled_digit_signals[decoded_digit.?] == null) {
+                remaining_digits -= 1;
+                scrambled_digit_signals[decoded_digit.?] = signal;
             }
         }
-
-        // Try if this mapping works...
-        generatePermutation(7, &mapping);
     }
 
-    // mapping {1,2,3,4}
-    // mapping {2,1,3,4}
+    // Use mapping to translate output.
+    var unscrambled_output: [4]u8 = undefined;
+    for (output) |out_signal, i_out| {
+        for (scrambled_digit_signals) |scrambled_signal_for_digit, i| {
+            if (out_signal == scrambled_signal_for_digit) {
+                unscrambled_output[i_out] = @intCast(u8, i);
+            }
+        }
+    }
 
-    //while (it.next()) |signal| {
-    //    // Find the digits we can deduce based only on len.
-    //    var easy_digit: ?u8 = switch (signal.len) {
-    //        2 => 1,
-    //        3 => 7,
-    //        4 => 4,
-    //        7 => 8,
-    //        else => null,
-    //    };
-    //    log.info("{s}\t: 0b{b:0>7} ({d})", .{ signal, signalStrToBitMap(signal), easy_digit });
-
-    //    // Try to find another signal that matches in all bits except for one.
-    //}
-
-    // Intersect the bits for the easy digits""
-
-    //for (mapping) |m, i| log.info("{} -> {d} ", .{ m, i });
-
-    //var signals_str = it.next();
-    //var output_digits_str = it.next();
-
+    var combined_output: u32 = 0;
+    for (unscrambled_output) |o| {
+        combined_output = combined_output * 10 + o;
+    }
+    return combined_output;
 }
 
 // [10 unique signal patterns] | [4 digit output]
@@ -224,22 +171,21 @@ const example =
     \\egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
     \\gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 ;
+var short_example = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
 
 test "part 1" {
     var num_easy_digits: u32 = countNumberOfEasyDigits(example);
     try testing.expectEqual(num_easy_digits, 26);
 }
 
-test "mapping" {
-    var r = mapSignal(0b1000000, [_]u8{ 2, 1, 0, 3, 4, 5, 6 });
-    std.debug.print("{b:0>7}", .{r});
-    try testing.expectEqual(r, 0b0010000);
-}
-
-var short_example_easy = "ab dab | cdbaf";
-var short_example = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
 test "part 2" {
-    //var num_easy_digits: u32 = countNumberOfEasyDigits(example);
-    //try testing.expectEqual(, 5353);
-    //try testing.expectEqual(, 61229);
+    var digits_short = deduceOutput(short_example);
+    try testing.expectEqual(digits_short, 5353);
+
+    var sum: u32 = 0;
+    var line_it = std.mem.split(example, "\n");
+    while (line_it.next()) |line| {
+        sum += deduceOutput(line);
+    }
+    try testing.expectEqual(sum, 61229);
 }
