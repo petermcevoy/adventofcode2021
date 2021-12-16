1b148eb7ac498d84d915823d889e7a90d313ba8e
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-12-16 22:12:27 +0100

    Day 10 part 2.

--- src/day10.zig
+++ src/day10.zig
@@ -6,13 +6,28 @@ const log = std.log.scoped(.day10);
 pub fn run() anyerror!void {
     var input_str = @embedFile("../data/day10_input.txt");
     var line_it = std.mem.tokenize(input_str, "\n");
-    var score: u32 = 0;
+
+    var score_corrupt: u64 = 0;
+
+    var num_fix_scores: usize = 0;
+    var score_fix_buffer = [_]u64{0} ** 100;
+    var buffer: [16]u8 = undefined;
     while (line_it.next()) |line| {
         if (findCorruptingChar(line)) |c| {
-            score += getCharScore(c);
+            score_corrupt += getIllegalCharScore(c);
+        }
+
+        if (fixIncompleteLine(line, &buffer)) |completing_chars| {
+            for (completing_chars) |c| {
+                score_fix_buffer[num_fix_scores] = score_fix_buffer[num_fix_scores] * 5 + getFixCharScore(c);
+            }
+            num_fix_scores += 1;
         }
     }
-    log.info("Part 1: score {d}", .{score});
+    log.info("Part 1: score_corrupt {d}", .{score_corrupt});
+
+    std.sort.sort(u64, score_fix_buffer[0..num_fix_scores], {}, comptime std.sort.asc(u64));
+    log.info("Part 2: score_fix {d}", .{score_fix_buffer[num_fix_scores / 2]});
 }
 
 const CharType = enum { opening, closing, unknown };
@@ -29,7 +44,7 @@ pub fn getMatchingClosingChar(c: u8) u8 {
     };
 }
 
-pub fn getCharScore(c: u8) u32 {
+pub fn getIllegalCharScore(c: u8) u32 {
     return switch (c) {
         ')' => 3,
         ']' => 57,
@@ -39,9 +54,19 @@ pub fn getCharScore(c: u8) u32 {
     };
 }
 
-pub fn findCorruptingChar(str: []const u8) ?u8 {
-    const stack_size = 16;
-    var char_stack = [_]u8{0} ** stack_size;
+pub fn getFixCharScore(c: u8) u64 {
+    return switch (c) {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        else => unreachable,
+    };
+}
+
+const stack_size = 16;
+pub fn processChars(str: []const u8, char_stack: *[stack_size]?u8) ?u8 {
+    char_stack.* = [_]?u8{null} ** stack_size;
     var i_stack: usize = 0;
 
     for (str) |c| {
@@ -69,7 +94,9 @@ pub fn findCorruptingChar(str: []const u8) ?u8 {
             },
             .closing => {
                 i_stack -= 1;
-                var latest_opening = char_stack[i_stack];
+                var latest_opening = char_stack[i_stack].?;
+                char_stack[i_stack] = null;
+
                 var expected_closing = getMatchingClosingChar(latest_opening);
                 if (c != expected_closing) return c; // Found illegal char!
             },
@@ -78,8 +105,30 @@ pub fn findCorruptingChar(str: []const u8) ?u8 {
 
         assert(i_stack < stack_size);
     }
-    // No errors for input!
     return null;
+}
+
+pub fn findCorruptingChar(str: []const u8) ?u8 {
+    var char_stack = [_]?u8{null} ** stack_size;
+    var illegal_char: ?u8 = processChars(str, &char_stack);
+    return illegal_char;
+}
+
+pub fn fixIncompleteLine(str: []const u8, buffer: *[16]u8) ?[]u8 {
+    var char_stack = [_]?u8{0} ** stack_size;
+    var illegal_char = processChars(str, &char_stack);
+    if (illegal_char != null) return null; // Can't fix, the chunk is illegal.
+
+    var i: usize = 0;
+    var num_chars: usize = 0;
+    while (i < 16) : (i += 1) {
+        if (char_stack[15 - i]) |c| {
+            (buffer.*)[num_chars] = getMatchingClosingChar(c);
+            num_chars += 1;
+        }
+    }
+
+    return if (num_chars > 0) buffer[0..num_chars] else null;
 }
 
 const example =
@@ -104,8 +153,23 @@ test "part 1" {
     while (line_it.next()) |line| {
         var char = findCorruptingChar(line);
         try testing.expectEqual(char, exptected_illegal_chars[i]);
-        if (char) |c| score += getCharScore(c);
+        if (char) |c| score += getIllegalCharScore(c);
         i += 1;
     }
     try testing.expectEqual(score, 26397);
+}
+
+test "part 2" {
+    var line_it = std.mem.tokenize(example, "\n");
+    var buffer: [16]u8 = undefined;
+    try testing.expectEqualStrings(fixIncompleteLine(line_it.next().?, &buffer).?, "}}]])})]");
+    try testing.expectEqualStrings(fixIncompleteLine(line_it.next().?, &buffer).?, ")}>]})");
+    try testing.expectEqual(fixIncompleteLine(line_it.next().?, &buffer), null);
+    try testing.expectEqualStrings(fixIncompleteLine(line_it.next().?, &buffer).?, "}}>}>))))");
+    try testing.expectEqual(fixIncompleteLine(line_it.next().?, &buffer), null);
+    try testing.expectEqual(fixIncompleteLine(line_it.next().?, &buffer), null);
+    try testing.expectEqualStrings(fixIncompleteLine(line_it.next().?, &buffer).?, "]]}}]}]}>");
+    try testing.expectEqual(fixIncompleteLine(line_it.next().?, &buffer), null);
+    try testing.expectEqual(fixIncompleteLine(line_it.next().?, &buffer), null);
+    try testing.expectEqualStrings(fixIncompleteLine(line_it.next().?, &buffer).?, "])}>");
 }
