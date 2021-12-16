1edcb98baa4f2b3b0bb110fd6cf45e7629dbef14
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-12-16 17:25:57 +0100

    Day 9 part 2.

--- src/day09.zig
+++ src/day09.zig
@@ -6,8 +6,42 @@ const log = std.log.scoped(.day09);
 pub fn run() anyerror!void {
     var input_str = @embedFile("../data/day09_input.txt");
     var map = HeightMap(100, 100).fromStr(input_str);
-    var risk_level = calcLowPointRiskLevels(&map);
-    log.info("Part 1: {d}", .{risk_level});
+
+    var top3_basin_sizes = [_]u32{0} ** 3;
+    var sum_risk_level: u32 = 0;
+    processMap(&map, &sum_risk_level, &top3_basin_sizes);
+
+    log.info("Part 1: {d}", .{sum_risk_level});
+    log.info("Part 2: {d}: {d}", .{ top3_basin_sizes, top3_basin_sizes[0] * top3_basin_sizes[1] * top3_basin_sizes[2] });
+}
+
+pub fn processMap(map: anytype, out_sum_risk_level: ?*u32, out_top3_basin_sizes: ?*[3]u32) void {
+    var top3_basin_sizes = [_]?u32{null} ** 3;
+    var sum_risk_level: u32 = 0;
+
+    var y: i32 = 0;
+    while (y < map.map_height) : (y += 1) {
+        var x: i32 = 0;
+        while (x < map.map_width) : (x += 1) {
+            if (map.getLowPoint(x, y)) |h| {
+                sum_risk_level += h + 1;
+                var basin_size = findBasinSizeFromPoint(map, x, y);
+                for (top3_basin_sizes) |_, i| {
+                    if (top3_basin_sizes[i] == null or basin_size > top3_basin_sizes[i].?) {
+                        if (i == 0) top3_basin_sizes[1] = top3_basin_sizes[0];
+                        if (i == 0 or i == 1) top3_basin_sizes[2] = top3_basin_sizes[1];
+                        top3_basin_sizes[i] = basin_size;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if (out_sum_risk_level != null) out_sum_risk_level.?.* = sum_risk_level;
+    if (out_top3_basin_sizes != null) {
+        for (out_top3_basin_sizes.?) |*entry, i| entry.* = top3_basin_sizes[i].?;
+    }
 }
 
 pub fn HeightMap(comptime width: usize, comptime height: usize) type {
@@ -15,6 +49,7 @@ pub fn HeightMap(comptime width: usize, comptime height: usize) type {
         map_width: usize = width,
         map_height: usize = height,
         height_values: [width * height]u8,
+        basin_marks: [width * height]bool = [_]bool{false} ** (width * height),
 
         const Self = @This();
 
@@ -41,7 +76,7 @@ pub fn HeightMap(comptime width: usize, comptime height: usize) type {
             return self.height_values[@intCast(usize, y) * width + @intCast(usize, x)];
         }
 
-        pub fn isLowPoint(self: *const Self, x: i32, y: i32) bool {
+        pub fn getLowPoint(self: *const Self, x: i32, y: i32) ?u32 {
             var cmp_val = self.getHeight(x, y).?;
 
             var points_to_check: [4][2]i32 = undefined;
@@ -55,31 +90,64 @@ pub fn HeightMap(comptime width: usize, comptime height: usize) type {
                 if (val == null) continue;
 
                 if (val.? <= cmp_val) {
-                    return false;
+                    return null;
                 }
             }
 
             // Did not find a adjascent lower point, this is a low point.
+            return cmp_val;
+        }
+
+        pub fn setBasin(self: *Self, x: i32, y: i32) bool {
+            var ux = @intCast(usize, x);
+            var uy = @intCast(usize, y);
+            var was_already_set = self.basin_marks[uy * width + ux];
+            if (was_already_set) return false;
+
+            self.basin_marks[uy * width + ux] = true;
             return true;
         }
     };
 }
 
-pub fn calcLowPointRiskLevels(map: anytype) u32 {
-    var sum_risk_level: u32 = 0;
+pub fn findBasinSizeFromPoint(map: anytype, x: i32, y: i32) u32 {
+    // grow from this starting position up until reaching height 9.
+    const directions: [4][2]i32 = .{
+        [2]i32{ 0, 1 },
+        [2]i32{ 0, -1 },
+        [2]i32{ 1, 0 },
+        [2]i32{ -1, 0 },
+    };
 
-    var y: i32 = 0;
-    while (y < map.map_height) : (y += 1) {
-        var x: i32 = 0;
-        while (x < map.map_width) : (x += 1) {
-            if (map.isLowPoint(x, y)) {
-                var val = map.getHeight(x, y).?;
-                sum_risk_level += val + 1;
+    var num_points_marked_as_basin: u32 = 0;
+
+    // Include self
+    if (map.setBasin(x, y))
+        num_points_marked_as_basin += 1;
+
+    var start_height = map.getHeight(x, y).?;
+
+    for (directions) |dir| {
+        var prev_val = start_height;
+        var curr_x: i32 = x + dir[0];
+        var curr_y: i32 = y + dir[1];
+        var val = map.getHeight(curr_x, curr_y);
+
+        while (val != null and val.? > prev_val and val.? < 9) {
+            if (map.setBasin(curr_x, curr_y)) {
+                num_points_marked_as_basin += 1;
+
+                // Explore basin from this point
+                num_points_marked_as_basin += findBasinSizeFromPoint(map, curr_x, curr_y);
             }
+
+            curr_x += dir[0];
+            curr_y += dir[1];
+            val = map.getHeight(curr_x, curr_y);
         }
     }
 
-    return sum_risk_level;
+    return num_points_marked_as_basin;
 }
 
 const example =
@@ -92,6 +160,15 @@ const example =
 
 test "part 1" {
     var map = HeightMap(10, 5).fromStr(example);
-    var risk_level = calcLowPointRiskLevels(&map);
-    try testing.expectEqual(risk_level, 15);
+    var sum_risk_level: u32 = 0;
+    processMap(&map, &sum_risk_level, null);
+    try testing.expectEqual(sum_risk_level, 15);
+}
+
+test "part 2" {
+    // Find three largest basins
+    var map = HeightMap(10, 5).fromStr(example);
+    var top3_basin_sizes = [_]u32{0} ** 3;
+    processMap(&map, null, &top3_basin_sizes);
+    try testing.expectEqual(top3_basin_sizes, [3]u32{ 14, 9, 9 });
 }
