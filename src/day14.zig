d6e64c85197aed2b0e91c67b24f7dfc0fa44aac2
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-12-27 15:42:36 +0100

    Day 14 part 2

--- src/day14.zig
+++ src/day14.zig
@@ -6,52 +6,70 @@ const log = std.log.scoped(.day14);
 pub fn run() anyerror!void {
     var input_str = @embedFile("../data/day14_input.txt");
     var rules_buffer: [100]InsertionRule = undefined;
-    var result_buffer: [max_len]u8 = undefined;
-    var template = std.mem.tokenize(input_str, "\n").next().?;
     var rules = parseInsertionRulesFromStr(&rules_buffer, input_str[std.mem.indexOf(u8, input_str, "\n").?..]);
+    var template = std.mem.tokenize(input_str, "\n").next().?;
+    var state = makeStateFromTemplate(template, rules);
 
     var i: usize = 0;
-    while (i < 10) : (i += 1) {
-        template = processStep(&result_buffer, rules, template);
-    }
+    while (i < 10) : (i += 1) processStep(&state, rules);
+    log.info("Part 1: {d}", .{calcAnswer(state.counts)});
 
-    var most_common_count: u32 = undefined;
-    var least_common_count: u32 = undefined;
-    _ = getElementCounts(template, &most_common_count, &least_common_count);
-
-    log.info("Part 1: {d}", .{most_common_count - least_common_count});
+    while (i < 40) : (i += 1) processStep(&state, rules);
+    log.info("Part 2: {d}", .{calcAnswer(state.counts)});
 }
 
-const max_len = 20_000;
-pub fn processStep(buffer: []u8, rules: []InsertionRule, template: []const u8) []const u8 {
-    // template could be a slice into the buffer, we copy it just to be sure...
-    var template_copy_buffer: [max_len]u8 = undefined;
-    std.mem.copy(u8, &template_copy_buffer, template);
-    var template_copy = template_copy_buffer[0..template.len];
+const State = struct { pairs: [26][26]u64, counts: [26]u64 };
 
-    // Go through template and process
+pub fn makeStateFromTemplate(template: []const u8, rules: []InsertionRule) State {
+    var state = State{
+        .pairs = .{.{0} ** 26} ** 26,
+        .counts = .{0} ** 26,
+    };
+
     var i_template: usize = 0;
-    var i_new: usize = 0;
-    while (i_template < template_copy.len) : (i_template += 1) {
-        var c: u8 = template_copy[i_template];
-        var c_next: ?u8 = if (i_template + 1 == template_copy.len) null else template_copy[i_template + 1];
+    while (i_template < template.len) : (i_template += 1) {
+        var c: u8 = template[i_template];
+        var c_next: ?u8 = if (i_template + 1 == template.len) null else template[i_template + 1];
 
-        buffer[i_new] = c;
-        i_new += 1;
+        var index1 = c - @intCast(usize, 'A');
+        state.counts[index1] += 1;
 
         if (c_next == null) continue;
-
-        for (rules) |r| {
-            if (std.mem.eql(u8, &r.pair, &[2]u8{ c, c_next.? })) {
-                // Insert the new char
-                buffer[i_new] = r.result;
-                i_new += 1;
-            }
-        }
-        buffer[i_new] = c_next.?;
+        var index2 = c_next.? - @intCast(usize, 'A');
+        state.pairs[index1][index2] += 1;
     }
 
-    return buffer[0..i_new];
+    return state;
+}
+
+pub fn processStep(state: *State, rules: []InsertionRule) void {
+    var state_pairs_copy: [26][26]u64 = state.pairs;
+
+    var i_c1: usize = 0;
+    while (i_c1 < 26) : (i_c1 += 1) {
+        var c1: u8 = @intCast(u8, i_c1) + 'A';
+        var i_c2: usize = 0;
+        while (i_c2 < 26) : (i_c2 += 1) {
+            if (state_pairs_copy[i_c1][i_c2] == 0) continue;
+            var c2: u8 = @intCast(u8, i_c2) + 'A';
+            for (rules) |r| {
+                if (std.mem.eql(u8, &r.pair, &[2]u8{ c1, c2 })) {
+                    // Where adding a new element based on the rule.
+                    var i_c_new = r.result - 'A';
+
+                    // Add the new pairs
+                    state.pairs[i_c1][i_c_new] += state_pairs_copy[i_c1][i_c2];
+                    state.pairs[i_c_new][i_c2] += state_pairs_copy[i_c1][i_c2];
+
+                    // By inserting a new charachter inbetween, we have split the previous pair.
+                    state.pairs[i_c1][i_c2] -= state_pairs_copy[i_c1][i_c2];
+
+                    // Keep track of the number of elemnts.
+                    state.counts[i_c_new] += state_pairs_copy[i_c1][i_c2];
+                }
+            }
+        }
+    }
 }
 
 const InsertionRule = struct {
@@ -79,31 +97,22 @@ pub fn parseInsertionRulesFromStr(buffer: []InsertionRule, str: []const u8) []In
     return buffer[0..num_rules];
 }
 
-pub fn getElementCounts(buffer: []const u8, most_common_count: ?*u32, least_common_count: ?*u32) [26]u32 {
-    var counts = [_]u32{0} ** 26;
-    for (buffer) |c| {
-        var index = c - 'A';
-        counts[index] += 1;
-    }
+pub fn calcAnswer(counts: [26]u64) u64 {
+    var most_common_count: u64 = 0;
+    var least_common_count: u64 = 0;
 
-    if (least_common_count != null or most_common_count != null) {
-        var counts_sorted: [26]u32 = undefined;
-        std.mem.copy(u32, &counts_sorted, &counts);
-        std.sort.sort(u32, &counts_sorted, {}, comptime std.sort.asc(u32));
-        if (most_common_count) |pcount| {
-            pcount.* = counts_sorted[25];
-        }
-        if (least_common_count) |pcount| {
-            for (counts_sorted) |c| {
-                if (c > 0) {
-                    pcount.* = c;
-                    break;
-                }
-            }
+    var counts_sorted: [26]u64 = undefined;
+    std.mem.copy(u64, &counts_sorted, &counts);
+    std.sort.sort(u64, &counts_sorted, {}, comptime std.sort.asc(u64));
+    most_common_count = counts_sorted[25];
+    for (counts_sorted) |c| {
+        if (c > 0) {
+            least_common_count = c;
+            break;
         }
     }
 
-    return counts;
+    return most_common_count - least_common_count;
 }
 
 const example =
@@ -131,20 +140,37 @@ test "part 1" {
     var template = std.mem.tokenize(example, "\n").next().?;
     var rules_buffer: [100]InsertionRule = undefined;
     var rules = parseInsertionRulesFromStr(&rules_buffer, example[std.mem.indexOf(u8, example, "\n").?..]);
-    var result_buffer: [max_len]u8 = undefined;
-    var result = processStep(&result_buffer, rules, template);
-    try testing.expectEqualStrings(result, "NCNBCHB");
-    result = processStep(&result_buffer, rules, result);
-    try testing.expectEqualStrings(result, "NBCCNBBBCBHCB");
 
-    var i: usize = 2;
-    while (i < 10) : (i += 1) {
-        result = processStep(&result_buffer, rules, result);
-    }
+    var state = makeStateFromTemplate(template, rules); // NNCB
+    try testing.expectEqualSlices(u64, &state.counts, &.{ 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
 
-    var most_common_count: u32 = undefined;
-    var least_common_count: u32 = undefined;
-    _ = getElementCounts(result, &most_common_count, &least_common_count);
-    try testing.expectEqual(most_common_count, 1749);
-    try testing.expectEqual(least_common_count, 161);
+    var i: usize = 0;
+    std.debug.print("\nProcessing it 1\n", .{});
+    processStep(&state, rules); // NCNBCHB
+    i += 1;
+    try testing.expectEqualSlices(u64, &state.counts, &.{ 0, 2, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+    std.debug.print("\nProcessing it 2\n", .{});
+    processStep(&state, rules); // NBCCNBBBCBHCB
+    i += 1;
+    try testing.expectEqualSlices(u64, &state.counts, &.{ 0, 6, 4, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+    std.debug.print("\nProcessing it 3\n", .{});
+    processStep(&state, rules); // NBBBCNCCNBBNBNBBCHBHHBCHB
+    i += 1;
+    try testing.expectEqualSlices(u64, &state.counts, &.{ 0, 11, 5, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+    while (i < 10) : (i += 1) processStep(&state, rules);
+    try testing.expectEqual(calcAnswer(state.counts), 1749 - 161);
+}
+
+test "part 2" {
+    var template = std.mem.tokenize(example, "\n").next().?;
+    var rules_buffer: [100]InsertionRule = undefined;
+    var rules = parseInsertionRulesFromStr(&rules_buffer, example[std.mem.indexOf(u8, example, "\n").?..]);
+
+    var state = makeStateFromTemplate(template, rules); // NNCB
+    var i: usize = 0;
+    while (i < 40) : (i += 1) processStep(&state, rules);
+    try testing.expectEqual(calcAnswer(state.counts), 2188189693529);
 }
